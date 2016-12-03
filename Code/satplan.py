729f90dcaa62fd6b83704d2a73f2f231039137f6
Diogo Roma Ferreira <droma@localhost.localdomain>
2016-12-03 14:53:22 +0000

    General organization.

--- Code/satplan.py
+++ Code/satplan.py
@@ -1,75 +1,105 @@
 #!/usr/bin/python
 
+# python libs
+import sys
+from copy import deepcopy
+
+# user scripts
 from general import *
 
-import sys
-
+# Class definitions
 class Atom:
-	def __init__(self,a_name,t_list,n):
-		self.name =  a_name		
+	def __init__(self, a_name, t_list, n):
+		self.name =  a_name
 		self.terms_list = t_list
-		# 0 negative 1 not negative
-		self.negative = n
+		# False negative, True not negative
+		self.negative = None
 
 class Action:
-	def __init__(self,ac_name,arg,p_list,e_list):
-		self.name = ac_name	# action name 
-		self.arg_list = arg # arguments list	
-		self.precond_list = p_list # list of atoms
-		self.effect_list = e_list # list of atoms
+	def __init__(self, ac_name, arg, p_list, e_list):
+		self.name = ac_name			# action name
+		self.arg_list = arg			# arguments list
+		self.precond_list = p_list 	# list of atoms
+		self.effect_list = e_list 	# list of atoms
 
 class State:
-	def __init__(self,t,a_list):
-		self.time = t
-		self.atoms_list = a_list # list of atoms
+	def __init__(self, t, a_list):
+		self.time = t				# depth of node
+		self.atoms_list = a_list 	# list of atoms
 
-
+# Functions
+# Reads an atom string and returns an atom class
 def read_atom(atom):
-	terms_list = []
-	atom = atom[:-1]
 
-	part = atom.split('(')
+	# splits string in the atom name (before '(') and terms (between '()')
+	name = atom[ : atom.find('(') ]
+	term_str = atom[ atom.find('(') + 1 : atom.find(')') ]
 
-	first_part = part[0]
-	
-	if first_part[0] == '-':
-		negative = 0
-		name = first_part[1:]
+	# identifies if the atom is negated
+	if name[0] == '-':
+		negative = True
+		name = name[1:]
 	else:
-		negative = 1
-		name = first_part	
+		negative = False
 
-	terms_part = part[1]	
-	terms = terms_part.split(',')
+	# separates terms in a list
+	terms = term_str.split(',')
 
-	for term in terms:
-		terms_list.append(term)
-
-	return (name,terms_list,negative)
+	return Atom(name, terms, negative)
 
 def read_action_name(action):
+
 	arg_dict = {}
-	action = action[:-1]
+
+	# splits string in the atom name (before '(') and terms (between '()')
+	name = action[ : action.find('(') ]
+	arg_str = action[ action.find('(') + 1 : action.find(')') ]
+
+	arg_list = arg_str.split(',')
+
 	n = 0
-
-	part = action.split('(')
-
-	name = part[0]
-
-	arg_part = part[1]
-
-	arg = arg_part.split(',')
-
-	for a in arg:
+	for a in arg_list:
 		arg_dict[a] = n
 		n = n + 1
 
-	return (name,arg_dict)
+	return (name, arg_dict)
 
+# Will read generic state line (initial or goal), returning the atom list and
+#updating the constant_list
+def read_state_line(line, constants_list):
+	atoms_list = []
+
+	for a in line[1:]:
+		atom = read_atom(a)
+		for t in atom.terms_list:
+			constants_list.add(t)
+		atoms_list.append(atom)
+
+	return atoms_list, constants_list
+
+# fills list of atoms in an action line (preconds and effects)
+def fills_act_list(in_list, arg_list):
+
+	out_list = []
+	for a in in_list:
+		atom = read_atom(a)
+
+		# replaces literals defined in actions arguments with a number
+		new_term = []
+		for t in atom.terms_list:
+			if t in arg_list:
+				new_term.append(arg_list.index(t))
+			else:
+				new_term.append(t)
+
+		atom.terms_list = deepcopy(new_term)
+		out_list.append(atom)
+
+	return out_list
 
 def read_data(file_name):
 
-	# Sedundo prof, se nao derem I temos de negar tudo ??? <-------------
+	# Segundo prof, se nao derem I temos de negar tudo ??? <-------------
 	initial = None
 	actions_list = []
 	constants_list = set([])
@@ -78,88 +108,62 @@ def read_data(file_name):
 		for line in f:
 			line_terms = line.split()
 
+			# initial state line
 			if line[0] == 'I':
-				atoms_list = []
+				atoms_list, constants_lists = read_state_line(line_terms, \
+					constants_list)
+				initial = State(0, atoms_list)
 
-				for a in line_terms[1:]:
-					(name,terms_list,negative) = read_atom(a)
-					for t in terms_list:
-						constants_list.add(t)
-					atoms_list.append(Atom(name,terms_list,negative))
-
-
-				initial = State(0,atoms_list)
-
+			# goal state line
 			elif line[0] == 'G':
-				atoms_list = []
-				for a in line_terms[1:]:
-					(name,terms_list,negative) = read_atom(a)
-					for t in terms_list:
-						constants_list.add(t)
-					atoms_list.append(Atom(name,terms_list,negative))
-				goal = State(-1,atoms_list)	
+				atoms_list, constants_lists = read_state_line(line_terms, \
+					constants_list)
+				goal = State(-1, atoms_list)
 
+			# action line
 			elif line[0] == 'A':
-					(action_name,arg_dict) = read_action_name(line_terms[1])
+				atom = read_atom(line_terms[1])
 
-					arg_list = []
-					for k,v in arg_dict.items():
-						arg_list.append(v)
+				arg_list = atom.terms_list
+				action_name = atom.name
 
-					n_point = 0
-					for term in line_terms:
-						if term == '->':
-							break
-						n_point = n_point + 1	
+				# finds point at which the preconds definition ends and the
+				#effects definition starts
+				n_point = 0
+				for term in line_terms:
+					if term == '->':
+						break
+					n_point = n_point + 1
 
+				# ^ dificil de ler sem saber ja a estrutura da linha, mudar
+				#depois
 
-					precond_list = []
-					for a in line_terms[3:n_point]:
-						(name,terms_list,negative) = read_atom(a)
-						new_term = []
-						for t in terms_list:
-							if arg_dict.has_key(t):
-								new_term.append(arg_dict[t])
-							else:
-								new_term.append(t)
+				# gets list of precond atoms and effect atoms
+				precond_list = fills_act_list(line_terms[3:n_point], arg_list)
+				effect_list = fills_act_list(line_terms[n_point+1:], arg_list)
 
-						precond_list.append(Atom(name,new_term,negative))
-					
-					effect_list = []
-					for a in line_terms[n_point+1:]:
-						(name,terms_list,negative) = read_atom(a)
-						new_term = []
-						for t in terms_list:
-							if arg_dict.has_key(t):
-								new_term.append(arg_dict[t])
-							else:
-								new_term.append(t)
-						#print new_term		
-						effect_list.append(Atom(name,new_term,negative))
+				actions_list.append(Action(action_name, arg_list, \
+				precond_list, effect_list))
 
-					
-					actions_list.append(Action(action_name,arg_list,precond_list,effect_list))		
-
-	return (initial,goal,actions_list,constants_list)
+	return (initial, goal, actions_list, constants_list)
 
 
 """
 -----------------------------MAIN--------------------------------------
 """
 if len(sys.argv) != 2:
-    print("Number of arguments is wrong, please enter 1 argument.")
-    sys.exit(2)
+	print("Number of arguments is wrong, please enter 1 argument.")
+	sys.exit(2)
 
 file_name = sys.argv[1] # input file
- 
-(initial,goal,actions_list,constants_list) = read_data(file_name)
-# sat_solver(initial,goal,actions_list,constants_list) 
 
-general_algorithm(initial,goal,actions_list,constants_list)
+(initial, goal, actions_list, constants_list) = read_data(file_name)
+# sat_solver(initial,goal,actions_list,constants_list)
 
+general_algorithm(initial, goal, actions_list, constants_list)
 
 """
-# PRINTS  
+# PRINTS
 print ('--Initial State')
 for a in initial.atoms_list:
 	print (a.name + str(a.terms_list))
@@ -169,11 +173,11 @@ print ('--Goal State')
 for a in goal.atoms_list:
 	print (a.name + str(a.terms_list))
 
-print('--Actions')		
+print('--Actions')
 for a in actions_list:
 	print(a.name)
 
-print('--Constants')	
+print('--Constants')
 for c in constants_list:
 	print(c)
 """
