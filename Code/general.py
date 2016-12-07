d1ea6eacb631ac7281d38c6011bbcd2586619576
Beatriz Catarino <beacatarino94@gmail.com>
2016-12-07 23:22:18 +0000

    encoder funcional e leitura do resultado do sat solver

--- Code/general.py
+++ Code/general.py
@@ -158,7 +158,7 @@ def read_answer(SAT_dictionary,answer,action_keys):
 					action = v
 					str = action.id + ' '
 					for arg in v.arg_list:
-						str += arg
+						str += arg + ' '
 					print(str)
 			
 
@@ -177,18 +177,18 @@ def create_CNF(initial,goal,actions_list,constants_list,h):
 	(clause_list,SAT_variables_list,atoms_dictionary) = clause_from_state(initial.atoms_list,clause_list,SAT_variables_list,atoms_dictionary,t)
 	(SAT_variables_list,clause_list) = negated_atoms(atoms_dictionary,constants_list,SAT_variables_list,clause_list,t)
 
-	print('inicio ' + str(len(clause_list))	)
+	#print('inicio ' + str(len(clause_list))	)
 
 	# 2 - Goal state ----------------------------------------------------------
 	t = h
 	(clause_list,SAT_variables_list,atoms_dictionary) = clause_from_state(goal.atoms_list,clause_list,SAT_variables_list,atoms_dictionary,t)
-	(SAT_variables_list,clause_list) = negated_atoms(atoms_dictionary,constants_list,SAT_variables_list,clause_list,t)
+	#(SAT_variables_list,clause_list) = negated_atoms(atoms_dictionary,constants_list,SAT_variables_list,clause_list,t)
 
 	print('after goal ' + str(len(clause_list))	)
 
 
 	action_counter = 0
-	contradiction_counter = 0
+	
 	frame_counter = 0
 	action_5 = 0
 
@@ -197,20 +197,21 @@ def create_CNF(initial,goal,actions_list,constants_list,h):
 	for t in range(0, h):
 		list_actions = []
 		action_literals = []
-		for a in actions_list:
-			
+		for a in actions_list:			
+			contradiction_counter = 0
 			combin = product(constants_list, repeat = len(a.arg_list)) 
 			for c in combin:
-				print('------------' + a.name + str(c))
+				#print('------------' + a.name + str(c))
 				if (check_contradiction(a,c,t) == True): # there is a contradiction
 					"""
 					action = SAT_variable(a.name,list(c),t)
 					SAT_variables_list.add(action)
 					list_literals = [literal(action,False)]
 					clause_list.append(clause(list_literals))
-					contradiction_counter += 1			
+					
 					"""			
-					print('contradiction ' + a.name + str(c))
+					#print('contradiction ' + a.name + str(c))
+					contradiction_counter += 1			
 				else:	
 					length_before = len(clause_list)
 					# 3 - actions imply both their preconditions and their effects, for all time   
@@ -262,11 +263,13 @@ def create_CNF(initial,goal,actions_list,constants_list,h):
 								clause_list.append(clause(list_literals))
 								frame_counter = frame_counter + 2	
 
+			#print('contradiction: ' + str(contradiction_counter))
 
 		# 5 - exactly one action is performed in each time step 
 		clause_list.append(clause(action_literals)) # clause at least one		
 		action_5 += 1
-		# 	- Clauses At most one:	
+		# 	- Clauses At most one:
+
 		for c in combinations(list_actions, 2):
 			list_literals = []
 			for l in range(0,2):				
@@ -279,53 +282,9 @@ def create_CNF(initial,goal,actions_list,constants_list,h):
 	print('3 actions  ' + str(action_counter) + ' contradictions ' + str(contradiction_counter))
 	print('frame counter ' + str(frame_counter))
 	print('action 5: ' + str(action_5))
-	
+	print('final: '+str(len(clause_list)))
 
 
-	
-	# MISSING: COMO saber que um atomo nao e modificado. Olhamos para os precons, effects ou apenas variaveis?
-	# De resto, para cada atomo e so aplicar as clausulas como tenho nas notas	
-
-	"""	
-	for t in range(0,h):
-		for a in actions_list:
-			combin = product(constants_list, repeat = len(a.arg_list)) 
-			for c in combin:	
-				
-				if (check_contradiction(a,c,t) == True):
-					list_literals = [literal(SAT_variable(a.name,list(c),t),False)]
-					clause_list.append(clause(list_literals))						
-					print('contradiction ' + a.name + str(c))
-				else:
-					atoms_modified = []		
-					for atom in a.effect_list: # looking at the effects - which atoms were modified by action
-						arg_list = []
-						for term in atom.terms_list:
-							try:
-								val = int(term)
-								arg_list.append(c[term])					   
-							except ValueError:
-								arg_list.append(term)
-						atoms_modified.append(SAT_variable(atom.name,arg_list,t+1))
-						
-					for (k,v) in atoms_dictionary.items():
-						# Combinacoes possiveis para um atomo
-						comb = product(constants_list, repeat = int(v))
-						for cc in comb:
-							list_literals_n = [literal(SAT_variable(a.name,list(c),t),False)]
-							list_literals = [literal(SAT_variable(a.name,list(c),t),False)]
-							atom = SAT_variable(k,list(cc),t+1)
-							SAT_variables_list.add(atom)
-							SAT_variables_list.add(SAT_variable(a.name,list(c),t))
-							if atom not in atoms_modified: # Add 2 clauses
-								list_literals.append(literal(atom,True))
-								list_literals.append(literal(SAT_variable(k,list(cc),t),False))
-								SAT_variables_list.add(SAT_variable(k,list(cc),t))
-								list_literals_n.append(literal(atom,False))
-								list_literals_n.append(literal(SAT_variable(k,list(cc),t),True))
-								clause_list.append(clause(list_literals_n))						
-								clause_list.append(clause(list_literals))						
-		
 	"""
 	# print('after frame ' + str(len(clause_list)))	
 
@@ -336,7 +295,7 @@ def create_CNF(initial,goal,actions_list,constants_list,h):
 		print(str(n) + ' '+ str(v.id) + str(v.arg_list) + ' t= ' + str(v.t))
 	
 	
-	"""
+	
 	# Print of clauses
 
 	file = open("clause.txt", "w")
@@ -388,7 +347,7 @@ def general_algorithm(initial,goal,actions_list,constants_list):
 		"""	
 		answer = ''
 		
-		if h == 1:
+		if h == 3:
 			
 			#print(SAT_problem)			
 			file = open("SAT.txt", "w")
@@ -396,11 +355,11 @@ def general_algorithm(initial,goal,actions_list,constants_list):
 			file.write(SAT_problem)
 			file.close()
 
-			"""
-			answer = 'v -1 2 3 4 0'
+			
+			answer = 'v -1 -2 -3 -4 -5 -6 -7 8 -9 -10 -11 -12 -13 14 -15 16 17 -18 -19 -20 -21 -22 -23 -24 -25 -26 -27 -28 -29 -30 31 32 -33 -34 -35 -36 -37 -38 -39 40 -41 -42 -43 -44 -45 -46 -47 -48 -49 -50 -51 -52 -53 -54 -55 -56 -57 -58 -59 -60 -61 -62 -63 -64 -65 -66 -67 -68 -69 -70 71 -72 -73 -74 -75 -76 -77 -78 79 -80 -81 -82 -83 -84 -85 -86 -87 -88 -89 -90 -91 -92 -93 -94 -95 96 97 98 -99 -100 -101 -102 -103 -104 -105 -106 -107 -108 -109 -110 -111 -112 -113 -114 -115 -116 -117 -118 -119 -120 -121 -122 -123 -124 -125 -126 -127 -128 -129 130 131 -132 -133 -134 -135 -136 137 -138 -139 -140 -141 -142 -143 -144 -145 -146 -147 -148 -149 -150 -151 -152 -153 -154 -155 -156 -157 -158 -159 -160 -161 -162 163 164 -165 -166 -167 -168 -169 -170 -171 -172 -173 -174 -175 -176 -177 -178 -179 -180 -181 -182 -183 -184 -185 -186 -187 -188 -189 -190 -191 -192 -193 -194 -195 -196 -197 -198 -199 -200 -201 -202 -203 204 -205 -206 -207 -208 -209 -210 211 -212 -213 -214 -215 -216 -217 -218 -219 -220 -221 -222 -223 -224 -225 -226 -227 -228 -229 -230 -231 -232 -233 -234 235 236 -237 -238 239 -240 -241 -242 -243 -244 -245 -246 -247 -248 -249 -250 -251 -252 -253 -254 -255 -256 -257 -258 -259 -260 0'
 			
 			read_answer(SAT_dictionary,answer[1:],action_keys)	
-			"""
+			
 
 			break
 		h = h + 1
