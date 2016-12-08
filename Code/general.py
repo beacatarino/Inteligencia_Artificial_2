9ce80b89b23e6b54edfe06e84d98957f4729f255
Diogo Roma Ferreira <diogomiguel0@hotmail.com>
2016-12-08 22:48:57 +0000

    Exports SAT Problems as a file (list of strings)

--- Code/general.py
+++ Code/general.py
@@ -34,27 +34,12 @@ def general_algorithm(initial,goal,actions_list,constants_list,atoms_dictionary)
 
 		SAT_problem,SAT_dict,action_keys = encoder_main(initial,goal,actions_list,constants_list,h,atoms_dictionary)
 
-		"""
-		answer = sat_solver_algorithm(SAT_problem)
+		print(SAT_problem)
+		answer = sat_solver(SAT_problem)
 
 		if answer != False:
 			break
 		else:
 			h = h + 1	
-		"""	
-		answer = ''
-		if h == 2:
-			
-			file = open("SAT.txt", "w")
-			file.write(SAT_problem)
-			file.close()
-			
-			# answer = 'v -1 -2 -3 -4 -5 -6 7 -8 9 10 11 12 13 14 15 -16 -17 -18 -19 -20 21 -22 23 -24 -25 -26 -27 -28 -29 30 31 32 -33 34 -35 -36 -37 -38 -39 0'
-			
-			# read_answer(SAT_dict,answer,action_keys)	
-			
-
-			break
-		h = h + 1
 
 	return answer
